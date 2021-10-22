17e6bb82ae5b748928af384769817a53a8444ae0
Andre Almeida <aralmeida99@gmail.com>
2021-10-22 16:49:47 +0100

    Inital work code for python client

---
+++ C1/pyRobot/mainRob.py
@@ -0,0 +1,161 @@
+import math
+import sys
+from croblink import *
+from math import *
+import xml.etree.ElementTree as ET
+
+CELLROWS = 7
+CELLCOLS = 14
+
+
+class MyRob(CRobLinkAngs):
+    def __init__(self, rob_name, rob_id, angles, host):
+        CRobLinkAngs.__init__(self, rob_name, rob_id, angles, host)
+
+    # In this map the center of cell (i,j), (i in 0..6, j in 0..13) is mapped to labMap[i*2][j*2].
+    #  to know if there is a wall on top of cell(i,j) (i in 0..5),
+    #  check if the value of labMap[i*2+1][j*2] is space or not
+    def setMap(self, labMap):
+        self.labMap = labMap
+
+    def printMap(self):
+        for l in reversed(self.labMap):
+            print(''.join([str(l) for l in l]))
+
+    def run(self):
+        if self.status != 0:
+            print("Connection refused or error")
+            quit()
+
+        state = 'stop'
+        stopped_state = 'run'
+
+        while True:
+            self.readSensors()
+
+            if self.measures.endLed:
+                print(self.rob_name + " exiting")
+                quit()
+
+            if state == 'stop' and self.measures.start:
+                state = stopped_state
+
+            if state != 'stop' and self.measures.stop:
+                stopped_state = state
+                state = 'stop'
+
+            if state == 'run':
+                if self.measures.visitingLed:
+                    state = 'wait'
+                if self.measures.ground == 0:
+                    self.setVisitingLed(True)
+                self.wander()
+            elif state == 'wait':
+                self.setReturningLed(True)
+                if self.measures.visitingLed:
+                    self.setVisitingLed(False)
+                if self.measures.returningLed == True:
+                    state = 'return'
+                self.driveMotors(0.0, 0.0)
+            elif state == 'return':
+                if self.measures.visitingLed == True:
+                    self.setVisitingLed(False)
+                if self.measures.returningLed == True:
+                    self.setReturningLed(False)
+                self.wander()
+
+    def wander(self):
+
+        # Radar IDs
+        center_id: int = 0
+        left_id: int = 1
+        right_id: int = 2
+        back_id: int = 3
+
+        # Proximity indexes
+        very_close: int = 3
+        close: int = 2
+        far: float = 1.0
+        very_far: float = 0.5
+
+        # Alternate movement indicators
+        lin: float = 1
+        rot: float = math.pi  # half rotation
+
+        if self.measures.irSensor[center_id] > 3.0 \
+                or self.measures.irSensor[right_id] > 3.0 \
+                or self.measures.irSensor[back_id] > 3.0:
+            print('Rotate left')
+            self.driveMotors(-0.1, +0.1)
+        elif self.measures.irSensor[left_id] > 3.0:
+            print('Rotate right')
+            self.driveMotors(+0.1, -0.1)
+        elif self.measures.irSensor[left_id] > 1:
+            print('Rotate slowly right')
+            self.driveMotors(0.1, 0.0)
+        elif self.measures.irSensor[right_id] > 1:
+            print('Rotate slowly left')
+            self.driveMotors(0.0, 0.1)
+        else:
+            print('Go')
+            self.driveMotors(0.2, 0.2)
+
+        in_l: float = lin - rot/2
+        in_r: float = rot + in_l
+        # TODO uncomment when decide to use lin/rot instead of each motor's power
+        # self.driveMotors(in_l, in_r)
+
+
+class Map():
+    def __init__(self, filename):
+        tree = ET.parse(filename)
+        root = tree.getroot()
+
+        self.labMap = [[' '] * (CELLCOLS * 2 - 1) for i in range(CELLROWS * 2 - 1)]
+        i = 1
+        for child in root.iter('Row'):
+            line = child.attrib['Pattern']
+            row = int(child.attrib['Pos'])
+            if row % 2 == 0:  # this line defines vertical lines
+                for c in range(len(line)):
+                    if (c + 1) % 3 == 0:
+                        if line[c] == '|':
+                            self.labMap[row][(c + 1) // 3 * 2 - 1] = '|'
+                        else:
+                            None
+            else:  # this line defines horizontal lines
+                for c in range(len(line)):
+                    if c % 3 == 0:
+                        if line[c] == '-':
+                            self.labMap[row][c // 3 * 2] = '-'
+                        else:
+                            None
+
+            i = i + 1
+
+
+rob_name = "pClient1"
+host = "localhost"
+pos = 1
+mapc = None
+
+for i in range(1, len(sys.argv), 2):
+    if (sys.argv[i] == "--host" or sys.argv[i] == "-h") and i != len(sys.argv) - 1:
+        host = sys.argv[i + 1]
+    elif (sys.argv[i] == "--pos" or sys.argv[i] == "-p") and i != len(sys.argv) - 1:
+        pos = int(sys.argv[i + 1])
+    elif (sys.argv[i] == "--robname" or sys.argv[i] == "-p") and i != len(sys.argv) - 1:
+        rob_name = sys.argv[i + 1]
+    elif (sys.argv[i] == "--map" or sys.argv[i] == "-m") and i != len(sys.argv) - 1:
+        mapc = Map(sys.argv[i + 1])
+    else:
+        print("Unkown argument", sys.argv[i])
+        quit()
+
+if __name__ == '__main__':
+    rob = MyRob(rob_name, pos, [0.0, 60.0, -60.0, 180.0], host)
+    if mapc is not None:
+        rob.setMap(mapc.labMap)
+        rob.printMap()
+
+    rob.run()
