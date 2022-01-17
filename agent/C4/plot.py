00b47b7923fffc1e81429e4f351f629d065a6bfd
Andre Almeida <aralmeida99@gmail.com>
2022-01-17 17:18:23 +0000

    Added 2 graphs, representing the difference between the real and estimated coordinates

--- agent/C4/plot.py
+++ agent/C4/plot.py
@@ -8,14 +8,29 @@ import pandas as pd
 class Main:
     data = pd.read_csv('pos.csv')
 
-    plt.subplot(2, 1, 1)
+    dev_x, dev_y = data.est_x - data.real_x, data.est_y - data.real_y
+    stats = tuple((np.min(dv), np.max(dv), np.std(dv), np.average(dv)) for dv in (dev_x, dev_y))
+    print(f"X deviation min/max/dev/avg = {stats[0]}")
+    print(f"Y deviation min/max/dev/avg = {stats[1]}")
+
+    plt.subplot(2, 2, 1)
     plt.plot(data.real_x, color='g')
     plt.plot(data.est_x, color='r')
     plt.title("X")
-    plt.subplot(2, 1, 2)
+
+    plt.subplot(2, 2, 2)
     plt.plot(data.real_y, color='g')
     plt.plot(data.est_y, color='r')
     plt.title("Y")
+
+    plt.subplot(2, 2, 3)
+    plt.plot(dev_x, color='b')
+    plt.title("X_dev")
+
+    plt.subplot(2, 2, 4)
+    plt.plot(dev_y, color='b')
+    plt.title("Y_dev")
+
     plt.show()
 
 
